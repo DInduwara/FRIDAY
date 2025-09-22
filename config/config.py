66b866fd62e7c1c6461c0e1d9ecc2bb26709258e
Dinuka Induwara <bandarainduwara3@gmail.com>
2025-09-22 10:34:14 +0530

    Add initial implementation of tools for weather, launches, and Todoist integration; include configuration and requirements setup

---
+++ config/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENWEATHER_API_KEY = os.getenv("WHEATHER_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+TODOIST_API_KEY = os.getenv("TODOIST_API_KEY")
+
+FULL_AI_TRACE= False
+
